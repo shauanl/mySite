b693f82b7b6cdae04cf946e2373fa18d2a2117df
shauanl <surferl@hotmail.com>
2024-11-15 20:50:34 -0500

    initial commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 export default function About() {
     return (
-        <h1>About</h1>
+        <>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem perspiciatis aut voluptatum explicabo eius doloribus quibusdam tempore corrupti tempora? Soluta repudiandae hic, provident repellat officiis ex aperiam praesentium dolorem fugiat.</p>
+        </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <h2>Home</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem perspiciatis aut voluptatum explicabo eius doloribus quibusdam tempore corrupti tempora? Soluta repudiandae hic, provident repellat officiis ex aperiam praesentium dolorem fugiat.</p>
+        </>
     )
 }
