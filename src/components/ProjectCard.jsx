8abbf3683d8b917e4008b30b92e5e39ece5ce854
shauanl <surferl@hotmail.com>
2024-11-26 20:19:57 -0500

    text adjusments for heading and subheading on projectCard component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,24 +1,26 @@
 import PropTypes from 'prop-types';
 
 
-export default function ProjectCard1({heading, subheading, descriptionOne, descriptionTwo }) {
+export default function ProjectCard1({subheading, descriptionOne, descriptionTwo }) {
     return (
-        <div className="mx-auto grid max-w-8xl grid-cold-1 gap-8 px-8 pb-24 pt-12 md:grid-cols-12">
-            <h2 className="col-span-1 text-3xl font-bold md:col-span-4 text-neutral-600">
-                {heading}
-                <br />
-                <span className="text-sm text-black">{subheading}</span>
+        <>
+            <span className="text-sm text-black ml-10">{subheading}</span>
+            <div className="mx-auto max-w-8xl grid-cold-1 gap-8 px-8 pb-24 pt-5 pl-10 pr-5 md:grid-cols-12">
+                {/* <h2 className="col-span-1 text-3xl font-bold md:col-span-4 text-neutral-600">
+                    {heading}
+                    <br />
 
-            </h2>
-            <div className="col-span-1 md:col-span-8 ibm-plex-sans-medium">
-                <p className="mb-4 text-xl text-black md:text-2xl">
-                    {descriptionOne}
-                </p>
-                <p className="mb-8 text-xl text-black md:text-2xl">
-                    {descriptionTwo}
-                </p>
+                </h2> */}
+                <div className="col-span-1 md:col-span-8 ibm-plex-sans-medium">
+                    <p className="mb-4 text-xl text-black md:text-2xl">
+                        {descriptionOne}
+                    </p>
+                    <p className="mb-8 text-xl text-black md:text-2xl">
+                        {descriptionTwo}
+                    </p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
