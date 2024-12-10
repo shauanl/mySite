06b387c1f777aa8b07736cae279181fc9832e937
shauanl <surferl@hotmail.com>
2024-12-09 20:46:44 -0500

    Add contanct form for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,16 +5,22 @@ export default function Contact() {
 
     return (
         <section className="section-contact -mt-1.25 section-work max-w-screen-xl mx-auto">
-            <h2 className="text-8xl text-left roboto-bold mb-20">Contact me</h2>
+            <h2 className="text-8xl text-left roboto-bold mb-20">Get in Touch</h2>
             <div className="contact-container flex">
                 <div className="contact-info w-1/2 flex-col">
-                    <p className="mt-10 block text-3xl">Get in Touch</p>
                     <p className="roboto-light mt-10 mb-10">I am available for hire and open to any ideas of cooperation.</p>
                     <p className="mt-10 mb-10">Email: shauanf@gmail.com</p>
-                    <p>Let's create seamless digital experiences—reach out </p>
                 </div>
                 <div className="contact-form w-1/2">
-
+                    <form className="flex flex-col">
+                        <label htmlFor="name" className="text-xl ibm-plex-sans-light">Name</label>
+                        <input required type="text" id="name" name="name" className="border border-gray-300 p-2 mb-4 roboto-regular" />
+                        <label htmlFor="email" className="text-xl ibm-plex-sans-light">Email</label>
+                        <input required type="email" id="email" name="email" className="border border-gray-300 p-2 mb-4 roboto-regular" />
+                        <label htmlFor="message" className="text-xl ibm-plex-sans-light">Message</label>
+                        <textarea required id="message" name="message" className="border border-gray-300 p-2 mb-4 roboto-regular"></textarea>
+                        <button className="roboto-regular bg-black text-white p-2 rounded-md">Send</button>
+                    </form>
                 </div>
             </div>
 
